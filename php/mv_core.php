c21d0bdcc07acf100b6d2b78e6ff4317e63e1ac0
CBarouch <github@HDWP.com>
2019-10-02 00:59:59 -0400

    Changed to a Folder layout

--- php/mv_core.php
+++ php/mv_core.php
@@ -1,14 +1,4 @@
 <?php
-echo mv_oconv(-1200,'D2/')  . "\r\n";
-echo mv_oconv(18500,'D2/')  . "\r\n";
-echo mv_oconv(18500,'D4-')  . "\r\n";
-echo mv_oconv(18500,'DM')  . "\r\n";
-echo mv_oconv(18500,'DD')  . "\r\n";
-echo mv_oconv(18500,'D2Y')  . "\r\n";
-echo mv_oconv(86375,'MTS') . "\r\n";
-echo mv_oconv(86375,'MTHS') . "\r\n";
-echo mv_oconv('A!BB!CCC!DDD!DDD','G1!3');
-
 // mv_core.php
 // by Charles Barouch
 // on 09/15/19
