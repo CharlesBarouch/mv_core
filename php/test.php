6eb836a072b24e9ffe3313ad4f4d8133d7a6e1a3
Charles Barouch <GitHub@HDWP.com>
2019-10-02 02:08:38 -0400

    Update test.php
    Added the ability to load a stack of test cases.

--- php/test.php
+++ php/test.php
@@ -1,6 +1,12 @@
 <?php
 include_once('./mv_core.php');
-
+$stack = file_get_contents('./teststack.tt');
+$stack = explode('^',$stack);
+foreach($stack as $testcase)
+{
+  $testcase = explode(']',$testcase);
+  echo mv_oconv($testcase[0],$testcase[1])  . "\r\n";
+}
 echo mv_oconv(-1200,'D2/')  . "\r\n";
 echo mv_oconv(18500,'D2/')  . "\r\n";
 echo mv_oconv(18500,'D4-')  . "\r\n";
