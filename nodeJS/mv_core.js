367c74193760c55da1a5d08e6699f44eb4316f0f
Aaron Young <brainomite@gmail.com>
2019-10-13 13:55:49 -0400

    Get date functions working

--- nodeJS/mv_core.js
+++ nodeJS/mv_core.js
@@ -1,5 +1,60 @@
 const mvOconv = (value, rule) => {
+  upcasedRule = rule.toUpperCase();
+  oneLetterRule = upcasedRule[0];
+  twoLetterRule = upcasedRule.substr(0, 2);
+
+  if (oneLetterRule === "D") {
+    return mvOconvDate(value, upcasedRule);
+  }
   return `${value}, ${rule}`;
+};
+
+const mvOconvDate = (value, rule) => {
+  const mvEpoch = new Date(1969, 0, 1);
+  let date = mvEpoch.addDays(value);
+
+  if (/D[1234][-\/]/.test(rule)) {
+    // regular expression for a full date with delimiters i.e. "D2-"
+    return getDate(date, rule);
+  } else if (/D[1234]Y/.test(rule)) {
+    // regular expression for year with a length i.e. "D4Y"
+    return getYear(date, rule);
+  } else if (rule === "DM") {
+    return pad(date.getMonth() + 1); // zero based months
+  } else if (rule === "DD") {
+    return pad(date.getDate());
+  } else {
+    return "oops";
+  }
+};
+
+const getYear = (date, rule) => {
+  years = date.getFullYear().toString();
+  chars = Number.parseInt(rule[1]);
+  return years.substring(4 - chars);
+};
+const getDate = (date, rule) => {
+  day = pad(date.getDate());
+  month = pad(date.getMonth() + 1); // zero-based months need to add 1
+  year = getYear(date, rule);
+  delim = rule[2];
+  return `${month}${delim}${day}${delim}${year}`;
+  // return "yay";
+};
+// helpers
+
+const pad = number => {
+  if (number < 10) {
+    return "0" + number;
+  }
+  return number.toString();
+};
+
+Date.prototype.addDays = function(days) {
+  // https://stackoverflow.com/questions/563406/add-days-to-javascript-date
+  var date = new Date(this.valueOf());
+  date.setDate(date.getDate() + days);
+  return date;
 };
 
 module.exports = {
--- nodeJS/test.js
+++ nodeJS/test.js
@@ -1,12 +1,11 @@
-var { mvOconv } = require("./mv_core");
+const { mvOconv } = require("./mv_core");
 
 console.log("Hardcoded Cases");
 console.log(mvOconv(18500, "D2/")); // 08/27/19
 console.log(mvOconv(18500, "D4-")); // 08-27-2019
-
-// console.log(mvOconv(18500, 'DM') // 08
-// console.log(mvOconv(18500, 'DD') // 27
-// console.log(mvOconv(18500, 'D2Y') // 19
-// console.log(mvOconv(86375, 'MTS') // 23:59:35
-// console.log(mvOconv(86375, 'MTHS') // 11:59:35pm
-// console.log(mvOconv('A!BB!CCC!DDD!DDD', 'G1!3') //["BB", "CCC", "DDD"]
+console.log(mvOconv(18500, "DM")); // 08
+console.log(mvOconv(18500, "DD")); // 27
+console.log(mvOconv(18500, "D2Y")); // 19
+// console.log(mvOconv(86375, "MTS")); // 23:59:35
+// console.log(mvOconv(86375, "MTHS")); // 11:59:35pm
+// console.log(mvOconv("A!BB!CCC!DDD!DDD", "G1!3")); //["BB", "CCC", "DDD"]
